2f7f92ea47a248b16a50f6ddf4604bcfc2747d22
bsgg <beatriz.s.g86@gmail.com>
2019-04-16 21:58:31 +0100

    First Commit

---
+++ ArchitectureVR/Source/ArchitectureVR/ArchitectureVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArchitectureVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHITECTUREVR_API AArchitectureVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
